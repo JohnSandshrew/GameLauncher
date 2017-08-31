aa5da134bf57cc32feeeab181316a88240eea8e2
Peter Kemp <pkemp1992@live.co.uk>
2017-08-31 19:25:58 +0100

    Created header with expanding hamburger menu, title, search bar, and menu

--- Game Launcher/Game Launcher/MainWindow.xaml.cs
+++ Game Launcher/Game Launcher/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using MaterialDesignColors;
 
 namespace Game_Launcher {
     /// <summary>
@@ -19,7 +11,21 @@ namespace Game_Launcher {
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
-            InitializeComponent();
+
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            //until we add StayOpen glag to Drawer this helps with scrollbar
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null) {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private void MenuPopupButton_OnClick(object sender, RoutedEventArgs e) {
+            return; //Just a return so the buttons dont error all over your face
+            }
         }
     }
-}
